dfbb052026fe48b041865e772987dfecd0e724de
Alexandre Génin <alexandre.genin@ens-lyon.fr>
2016-02-09 16:41:34 +0100

    Updated data conversion for cross-sections

--- data/convert_data_format.R
+++ data/convert_data_format.R
@@ -4,48 +4,86 @@
 # 
 
 # Base paths (with / at the end /!\)
-result_folder <- '/home/alex/caspr/draft/ifcam_results/'
+result_folder <- '/home/alex/work/2015-2016/SpatialStress/ifcam_output/data/' 
 setwd(result_folder)
 
+library(caspr)
+
+CONVERT_PD <- FALSE
+CONVERT_CS   <- TRUE
+
+# Function that actually does the conversion
 convert_dat <- function(upper_branch, lower_branch, newfile) { 
-  
+
   # Convert all matrices to binary_matrices (for loop = less memory used)
   for (i in seq.int(upper_branch[[2]])) { 
-    print(i)
+    cat('.')
     upper_branch[[2]][[i]] <- lapply(upper_branch[[2]][[i]], as.binary_matrix)
     lower_branch[[2]][[i]] <- lapply(lower_branch[[2]][[i]], as.binary_matrix)
   }
-  
+  cat('\n')
   save(upper_branch, lower_branch, file = newfile, compress = 'bzip2')
   
   rm(lower_branch, upper_branch)
 }
 
-# Convert forestgap 
-load(paste0(result_folder,'result_forestgap.rda'), verbose = TRUE)
-convert_dat(result_forestgap_upper, result_forestgap_lower, 
-            newfile = paste0(result_folder, "result_forestgap_processed.rda" ))
-rm(result_forestgap_upper, result_forestgap_lower)
-gc()
 
-# Convert predprey 
-# load(paste0(result_folder,'result_predprey.rda'), verbose = TRUE)
-# convert_dat(result_predprey_upper, result_predprey_lower, 
-#             newfile = paste0(result_folder, "result_predprey_processed.rda" ))
-# rm(result_predprey_upper, result_predprey_lower)
-# gc()
 
-# Convert musselbed
-load(paste0(result_folder,'result_musselbed.rda'), verbose = TRUE)
-convert_dat(result_musselbed_upper, result_musselbed_lower, 
-            newfile = paste0(result_folder, "result_musselbed_processed.rda" ))
-rm(result_musselbed_upper, result_musselbed_lower)
-gc()
 
-# Convert grazing
-load(paste0(result_folder,'result_grazing.rda'), verbose = TRUE)
-convert_dat(result_grazing_upper, result_grazing_lower, 
-            newfile = paste0(result_folder, "result_grazing_processed.rda" ))
-rm(result_grazing_upper, result_grazing_lower)
-gc()
 
+# Convert phase-diagrams data
+# ------------------------------------
+
+if ( CONVERT_PD ) { 
+
+  # Convert forestgap 
+  load(paste0(result_folder,'result_forestgap.rda'), verbose = TRUE)
+  convert_dat(result_forestgap_upper, result_forestgap_lower, 
+              newfile = paste0(result_folder, "result_forestgap_processed.rda" ))
+  rm(result_forestgap_upper, result_forestgap_lower)
+  gc()
+
+  # Convert musselbed
+  load(paste0(result_folder,'result_musselbed.rda'), verbose = TRUE)
+  convert_dat(result_musselbed_upper, result_musselbed_lower, 
+              newfile = paste0(result_folder, "result_musselbed_processed.rda" ))
+  rm(result_musselbed_upper, result_musselbed_lower)
+  gc()
+
+  # Convert grazing
+  load(paste0(result_folder,'result_grazing.rda'), verbose = TRUE)
+  convert_dat(result_grazing_upper, result_grazing_lower, 
+              newfile = paste0(result_folder, "result_grazing_processed.rda" ))
+  rm(result_grazing_upper, result_grazing_lower)
+  gc()
+
+}
+
+
+# Convert cross-section data
+# ------------------------------------
+
+if ( CONVERT_CS ) { 
+
+  # Convert musselbed
+  load(paste0(result_folder,'result_musselbed_cs.rda'), verbose = TRUE)
+  convert_dat(result_musselbed_upper, result_musselbed_lower, 
+              newfile = paste0(result_folder, "result_musselbed_cs_processed.rda" ))
+  rm(result_musselbed_upper, result_musselbed_lower)
+  gc()
+
+  # Convert forestgap
+  load(paste0(result_folder,'result_forestgap_cs.rda'), verbose = TRUE)
+  convert_dat(result_forestgap_upper, result_forestgap_lower, 
+              newfile = paste0(result_folder, "result_forestgap_cs_processed.rda" ))
+  rm(result_forestgap_upper, result_forestgap_lower)
+  gc()
+
+  # Convert grazing
+  load(paste0(result_folder,'result_grazing_cs.rda'), verbose = TRUE)
+  convert_dat(result_grazing_upper, result_grazing_lower, 
+              newfile = paste0(result_folder, "result_grazing_cs_processed.rda" ))
+  rm(result_grazing_upper, result_grazing_lower)
+  gc()
+
+}
